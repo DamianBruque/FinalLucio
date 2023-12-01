29310256f87fecc44287e1f99a563615eb89af92
DaamiAle <daamiale.recoil@gmail.com>
2023-12-01 11:15:25 -0300

    Codefirst con ORM EntityFramework

---
+++ Models/Post.vb
@@ -0,0 +1,15 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
+
+Public Class Post
+    <Key>
+    <DatabaseGenerated(DatabaseGeneratedOption.Identity)>
+    Public Property PostId As Integer
+    Public Property PersonId As Integer
+    <ForeignKey("PersonId")>
+    Public Property Person As Person
+    Public Property Title As String
+    Public Property Content As String
+    Public Property Image As String
+    Public Property DatePost As Date
+End Class
---
+++ Models/User.vb
@@ -0,0 +1,12 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
+
+Public Class User
+    <Key>
+    <DatabaseGenerated(DatabaseGeneratedOption.Identity)>
+    Public Property UserId As Integer
+    Public Property Email As String
+    Public Property Password As String
+    Public Property State As Boolean
+
+End Class
